dde96d54d3b8e84d85a54e0b037487835d5dbb2f
JeevanivasanRamasamy <65586291+JeevanivasanRamasamy@users.noreply.github.com>
2024-06-24 20:34:54 +0530

    Minor UI Fix (#3)
    merging both icon and color change as one single commit

--- NotesApp/ContentView.swift
+++ NotesApp/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
+                .foregroundColor(.green)
                 .font(.system(size: 50))
             Text("Hello, world!")
                 .padding(.top, 3)
