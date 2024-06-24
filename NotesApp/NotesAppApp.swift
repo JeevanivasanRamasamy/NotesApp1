594840519f74343807c754a2d3c1fac6b17b0472
Jeeva <jeeva.rk@zohocorp.com>
2024-06-24 18:51:16 +0530

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by jeeva-14880 on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
